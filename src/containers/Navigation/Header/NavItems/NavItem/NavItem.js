e4f7d567e40e36f606d55a19caa4197fc0dfc17c
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2020-11-02 19:59:34 +0530

    removed changeBg prop

--- src/containers/Navigation/Header/NavItems/NavItem/NavItem.js
+++ src/containers/Navigation/Header/NavItems/NavItem/NavItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './NavItem.module.css'
 const NavItem = (props) => {
     return (
-        <li className={props.changeColor ? classes.NavItem + ' ' + classes.active : classes.NavItem}>
+        <li className={classes.NavItem}>
             {props.name}
         </li>
 
--- src/containers/Navigation/Header/NavItems/SignInNav/SignInNav.js
+++ src/containers/Navigation/Header/NavItems/SignInNav/SignInNav.js
@@ -4,19 +4,17 @@ import classes from './SignInNav.module.css'
 import { Link } from "react-router-dom";
 import NavItem from '../NavItem/NavItem'
 
-function SignInNav({ changeBg }) {
+function SignInNav() {
     return (
 
         <div className={classes.NavSignIn}>
             <Link to="/signin">
-                <NavItem name="Sign In"
-                    changeColor={changeBg} />
+                <NavItem name="Sign In"/>
             </Link>
 
             <Link to="/register">
                 <NavItem
-                    name="Register"
-                    changeColor={changeBg} />
+                    name="Register"/>
             </Link>
         </div>
 
