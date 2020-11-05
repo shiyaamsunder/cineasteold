603ede87a54685939e44389438fbfcbfbca83579
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2020-11-05 13:30:07 +0530

    added skeletons

--- src/containers/Watchlist/WatchList.js
+++ src/containers/Watchlist/WatchList.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import SkeletonComponent from '../../components/UI/Skeletons/SkeletonComponent'
 import MovieCard from '../MovieComponents/MovieCard/MovieCard';
 import classes from './WatchList.module.css'
 import { useMovList } from '../../context/MovListContext'
@@ -16,14 +17,22 @@ const WatchList = () => {
         }
     }, [currentUser, setWatchList])
 
+    const arr = new Array(10)
+    for(let i=0; i<=10; i++){
+        arr.push(i)
+    }
+
     return (
         <div className={classes.WatchListContainer}>
+        {console.log(watchList)}
             <div className={classes.WatchList}>
                 <h2>Your Watchlist</h2>
                 <div className={classes.Movies}>
-                    {watchList.length < 1 ? <h1>Nothing here</h1> : watchList.map(movie => (
+                    {watchList && watchList.map(movie => (
                         <MovieCard url={movie.url} title={movie.title} rating={movie.rating} key={movie.id} id={movie.id} onHomepage={false} />
                     ))}
+
+                    {!watchList && arr.map(el=> <SkeletonComponent key={el}/>)}
 
                 </div>
 
