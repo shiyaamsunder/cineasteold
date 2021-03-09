8bc5ad7b56d865ceaf12c8dd65aa848016067da9
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2021-03-09 18:24:12 +0530

    updated version number

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import classes from "./Footer.module.css"
-import GitHubIcon from '@material-ui/icons/GitHub';
+import React from "react";
+import classes from "./Footer.module.css";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import { version } from "../../../package.json";
 
 const Footer = () => {
-    return (
-        <footer className={classes.footer}>
-            <div className={classes.footer__container}>
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/shiyaamsunder/MovList">
-            <GitHubIcon style={{fontSize: '2rem'}}/>
-            </a>
-                
-                <div className={classes.footer__info}>
-                <p>v1.0.0</p>
-                </div>
-            
-            </div>
-                
-        </footer>
-    )
-}
+	return (
+		<footer className={classes.footer}>
+			<div className={classes.footer__container}>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/shiyaamsunder/MovList"
+				>
+					<GitHubIcon style={{ fontSize: "2rem" }} />
+				</a>
 
-export default Footer
+				<div className={classes.footer__info}>
+					<p>v{version}</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
