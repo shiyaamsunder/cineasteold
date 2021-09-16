257b132a625f9f51f23a2970087b4c80728dc247
shiyaam <shiyaamsunderms@gmail.com>
2021-09-16 22:08:16 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,68 +1,64 @@
-import React from 'react';
-import './App.css';
+import React, { Suspense, lazy } from "react";
+import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Loading from "./components/UI/Loading/Loading";
+import Header from "./containers/Navigation/Header/Header";
+import Footer from "./containers/Footer/Footer";
+import Layout from "./components/Layout/Layout";
+import MainContent from "./containers/MainContent/MainContent";
+import { MovListProvider } from "./context/MovListContext";
+import { ToastContainer } from "react-toastify";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
-import { AuthProvider } from './context/AuthContext';
-import WatchList from './containers/Watchlist/WatchList';
-import Header from './containers/Navigation/Header/Header';
-import Footer from './containers/Footer/Footer'
-import Layout from './components/Layout/Layout';
-import MainContent from './containers/MainContent/MainContent'
-import SignIn from './containers/UserAuth/SignIn/SignIn'
-import Register from './containers/UserAuth/Register/Register'
-import { MovListProvider } from './context/MovListContext';
-import FullMovie from './containers/MovieComponents/FullMovie/FullMovie';
-import { ToastContainer } from 'react-toastify';
-import SearchComponent from './containers/SearchComponent/SearchComponent';
-
+const SignIn = lazy(() => import("./containers/UserAuth/SignIn/SignIn"));
+const Register = lazy(() => import("./containers/UserAuth/Register/Register"));
+const WatchList = lazy(() => import("./containers/Watchlist/WatchList"));
+const SearchComponent = lazy(() =>
+  import("./containers/SearchComponent/SearchComponent")
+);
+const FullMovie = lazy(() =>
+  import("./containers/MovieComponents/FullMovie/FullMovie")
+);
 
 function App() {
-
-
-
   return (
     <AuthProvider>
       <MovListProvider>
-
-
         <Router>
           <div className="App">
             <Layout>
-              <Route path='/' exact>
+              <Route path="/" exact>
                 <Header />
                 <MainContent />
-                <Footer/>
-              </Route >
-              <Switch>
-                <Route path="/movies/:movieId" >
-                  <Header />
-                  <FullMovie />
-                </Route>
-                <Route path="/signin" component={SignIn} />
-                <Route path="/register" component={Register} />
-                <Route path="/my-watchlist">
-                  <Header />
-                  <WatchList />
-                </Route>
-                <Route path="/search">
-                  <Header />
-                  <SearchComponent />
-                  <Footer/>
-                </Route>
-              </Switch>
+                <Footer />
+              </Route>
+
+              <Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route path="/movies/:movieId">
+                    <Header />
+                    <FullMovie />
+                  </Route>
+                  <Route path="/signin" component={SignIn} />
+                  <Route path="/register" component={Register} />
+                  <Route path="/my-watchlist">
+                    <Header />
+                    <WatchList />
+                  </Route>
+                  <Route path="/search">
+                    <Header />
+                    <SearchComponent />
+                    <Footer />
+                  </Route>
+                </Switch>
+              </Suspense>
             </Layout>
             <ToastContainer />
           </div>
         </Router>
       </MovListProvider>
     </AuthProvider>
-
-
   );
 }
 
---
+++ src/components/UI/Loading/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Loading.module.css";
+
+const Loading = () => {
+  return (
+    <div className={classes.Wrapper}>
+      <h2>Loading</h2>
+    </div>
+  );
+};
+
+export default Loading;
