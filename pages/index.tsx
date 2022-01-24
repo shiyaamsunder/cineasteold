33c238d7f5aef2cb223a7638949a97d5ffeaf046
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-01-24 21:09:25 +0530

    added eslint, husky & lintstaged

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Cineaste</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h1>Hello world</h1>
-    </div>
-  );
-};
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <title>Cineaste</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1>Hello world</h1>
+  </div>
+);
 
 export default Home;
