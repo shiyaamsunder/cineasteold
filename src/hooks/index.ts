6593497997da3b37348ef793fd34b5f36fdb53b9
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-02-23 21:49:44 +0530

    added Infinite Scroll

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { usePagination } from "./usePagination";
+export { useOnScreen } from "./useOnScreen";
---
+++ src/hooks/useOnScreen.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+export const useOnScreen = (
+  ref: Element | null | undefined,
+  options?: IntersectionObserverInit
+) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, [ref, options]);
+
+  return { isVisible };
+};
