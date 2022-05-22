1b034acf3dcd2298c6dd7557979cebc03da5f377
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-05-22 19:22:40 +0530

    added icons

---
+++ src/components/icons/burger.tsx
@@ -0,0 +1,17 @@
+export const BurgerIcon = () => (
+  <svg
+    className="burger"
+    width="30"
+    height="30"
+    viewBox="0 0 30 30"
+    aria-hidden="true"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="2"
+      d="M4 7h22M4 15h22M4 23h22"
+    />
+  </svg>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export * from "./layout";
 export * from "./movie-card";
 export * from "./navbar";
 export * from "./ui";
-export * from "../styles/pages";
+export * from "./icons";
