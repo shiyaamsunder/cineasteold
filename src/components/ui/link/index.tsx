6d69763853e581128f353c113a6992e49b78b52e
shiyaam <shiyaamsunderms@gmail.com>
2022-08-23 23:03:19 +0530

    chore: rest props

--- src/components/ui/link/index.tsx
+++ src/components/ui/link/index.tsx
@@ -8,13 +8,14 @@ export type ILink = {
   children: ReactNode;
   href: string;
   size?: ComponentSizeProps;
-} & ComponentPropsWithoutRef<"button">;
+} & ComponentPropsWithoutRef<"a">;
 
 const StyledLink = styled.a<ILink>`
   text-decoration: none;
   font-weight: 400;
   font-size: ${({ size }) => size && `${getProperFontSizeFromProp(size)}px`};
   color: ${color("purple.100")};
+  margin: 0px 5px;
   &:hover {
     color: ${color("purple.300")};
     text-decoration: underline;
@@ -24,9 +25,9 @@ const StyledLink = styled.a<ILink>`
     padding: 0;
   }
 `;
-export const Link = ({ children, size = "md" }: ILink) => {
+export const Link = ({ children, size = "md", href, ...rest }: ILink) => {
   return (
-    <StyledLink size={size} href="">
+    <StyledLink size={size} href={href} {...rest}>
       {children}
     </StyledLink>
   );
