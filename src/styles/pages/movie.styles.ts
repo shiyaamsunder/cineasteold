4a6bf1727f3444de86b57f95ce4f72fcd1dfd7b1
shiyaam <shiyaamsunderms@gmail.com>
2022-08-27 22:11:57 +0530

    fix: titleoverlay style fix

--- src/styles/pages/movie.styles.ts
+++ src/styles/pages/movie.styles.ts
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
 export const TitleOverlay = styled.div`
-position: absolute;
-top: 0;
-background:
-  linear-gradient(180deg; rgba(0,0,0,0) 16%, rgba(0,0,0,1) 78%),
-padding: 10px;
-height: 100%;
-width: 100%;
-display: flex;
-alignItems: flex-end;
-
+  position: absolute;
+  top: 0;
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 16%,
+    rgba(0, 0, 0, 1) 78%
+  );
+  padding: 10px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: flex-end;
 `;
 
 export const MoviePageContainer = styled.div`
