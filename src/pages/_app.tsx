5eb062e9c29ffd6002677e687b671cd494e045fb
shiyaam <shiyaamsunderms@gmail.com>
2022-09-07 11:28:25 +0530

    chore: removed trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { withTRPC } from "@trpc/next";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 
-import type { AppRouter } from "./api/trpc/[trpc]";
-
 import { Navbar, Layout } from "@components";
 import { AuthProvider, supabase } from "@utils";
 
-function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
+export default function MyApp({
+  Component,
+  pageProps: { ...pageProps },
+}: AppProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -28,33 +28,3 @@ function MyApp({ Component, pageProps: { ...pageProps } }: AppProps) {
     </Layout>
   );
 }
-
-export default withTRPC<AppRouter>({
-  config() {
-    /**
-     * If you want to use SSR, you need to use the server's full URL
-     * @link https://trpc.io/docs/ssr
-     */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
-
-    return {
-      url,
-      /**
-       * @link https://react-query.tanstack.com/reference/QueryClient
-       */
-      queryClientConfig: {
-        defaultOptions: {
-          queries: {
-            refetchOnMount: false,
-          },
-        },
-      },
-    };
-  },
-  /**
-   * @link https://trpc.io/docs/ssr
-   */
-  ssr: true,
-})(MyApp);
