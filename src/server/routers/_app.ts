57427207a34ca0273ca491ddd1741a7fe79f3eb7
shiyaam <shiyaamsunderms@gmail.com>
2022-09-08 12:53:21 +0530

    fix: remove trpc server

--- src/server/routers/_app.ts
+++
@@ -1,8 +0,0 @@
-import { users } from "./user";
-import { buckets } from "./bucket";
-
-import { createRouter } from "@server/createRouter";
-
-export const appRouter = createRouter()
-  .merge("user.", users)
-  .merge("bucket.", buckets);
